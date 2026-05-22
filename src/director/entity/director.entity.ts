36a362504f29f05084263d4df232489b07baad8f
hgim96715-lgtm <hgim96715@gmail.com>
2026-05-22 19:10:33 +0900

    fix: Director 엔티티 등록 누락 수정

--- src/director/entity/director.entity.ts
+++ src/director/entity/director.entity.ts
@@ -1,10 +1,11 @@
 import { BaseTable } from "src/common/entity/base-table.entity";
 import { Movie } from "src/movie/entity/movie.entity";
-import { Column, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 
 // 감독은 동명이인이 있을 수 있으니 name은 unique로 안하는 게 나을 것 같다.
 // dob date of birthday 약어 자주 사용
+@Entity()
 export class Director extends BaseTable {
     @PrimaryGeneratedColumn()
     id:number;
