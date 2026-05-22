af167e1c92232b3d473fb662732d82acb2889a2e
hgim96715-lgtm <hgim96715@gmail.com>
2026-05-22 22:11:48 +0900

    feat: Director name·dob 복합 유니크 제약 추가 동명이인은 허용하고, 이름과 생년월일이 같으면 같은 감독으로 간주하도록 DB 유니크 제약을 둔다.

--- src/director/entity/director.entity.ts
+++ src/director/entity/director.entity.ts
@@ -1,11 +1,14 @@
 import { BaseTable } from "src/common/entity/base-table.entity";
 import { Movie } from "src/movie/entity/movie.entity";
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from "typeorm";
 
 
 // 감독은 동명이인이 있을 수 있으니 name은 unique로 안하는 게 나을 것 같다.
 // dob date of birthday 약어 자주 사용
 @Entity()
+
+// [name,dob]를 묶어서 두개가 같은 사람은 이 프로젝트에서 같은 사람으로 간주하기 위해 사용
+@Unique('UQ_DIRECTOR_NAME_DOB',['name','dob'])
 export class Director extends BaseTable {
     @PrimaryGeneratedColumn()
     id:number;
