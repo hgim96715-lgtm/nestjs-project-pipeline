da2c3fedfcb9d9478c087a08be36f6bc5a524ea9
hgim96715-lgtm <hgim96715@gmail.com>
2026-05-26 11:56:59 +0900

    feat(director): 생성/수정/삭제 엔드포인트 RBAC 권한 제어 추가

--- src/director/director.controller.ts
+++ src/director/director.controller.ts
@@ -2,11 +2,14 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from 
 import { DirectorService } from './director.service';
 import { CreateDirectorDto } from './dto/create-director.dto';
 import { UpdateDirectorDto } from './dto/update-director.dto';
+import { RBAC } from 'src/auth/decorator/rbac.decorator';
+import { Role } from 'src/user/entity/user.entity';
 
 @Controller('director')
 export class DirectorController {
   constructor(private readonly directorService: DirectorService) {}
 
+  @RBAC(Role.admin)
   @Post()
   create(@Body() createDirectorDto: CreateDirectorDto) {
     return this.directorService.create(createDirectorDto);
@@ -22,11 +25,13 @@ export class DirectorController {
     return this.directorService.findOne(id);
   }
 
+  @RBAC(Role.paidUser)
   @Patch(':id')
   update(@Param('id',ParseIntPipe) id: number, @Body() updateDirectorDto: UpdateDirectorDto) {
     return this.directorService.update(id, updateDirectorDto);
   }
 
+  @RBAC(Role.admin)
   @Delete(':id')
   remove(@Param('id',ParseIntPipe) id: number) {
     return this.directorService.remove(id);
