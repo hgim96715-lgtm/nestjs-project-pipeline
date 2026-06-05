4fd8cf65c8798263889f7f975594adaad92104f8
hgim96715-lgtm <hgim96715@gmail.com>
2026-06-05 19:35:15 +0900

    feat(genre): GenreService 데이터 접근을 TypeORM에서 Prisma로 전환

--- src/genre/genre.module.ts
+++ src/genre/genre.module.ts
@@ -3,10 +3,11 @@ import { GenreService } from './genre.service';
 import { GenreController } from './genre.controller';
 import { Genre } from './entity/genre.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PrismaModule } from 'src/common/prisma.module';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Genre])],
-  controllers: [GenreController],
-  providers: [GenreService],
+    imports: [TypeOrmModule.forFeature([Genre]), PrismaModule],
+    controllers: [GenreController],
+    providers: [GenreService],
 })
 export class GenreModule {}
--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -4,72 +4,92 @@ import { UpdateGenreDto } from './dto/update-genre.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Not, Repository } from 'typeorm';
 import { Genre } from './entity/genre.entity';
+import { PrismaService } from 'src/common/prisma.service';
 
 @Injectable()
 export class GenreService {
-  constructor(@InjectRepository(Genre) private readonly genreRepository:Repository<Genre>){}
-  async create(createGenreDto: CreateGenreDto) {
-    const genre=await this.genreRepository.findOne({
-      where:{name:createGenreDto.name}
-    })
-    if(genre){
-      throw new NotFoundException('이미 존재하는 장르입니다.')
-    }
-    return this.genreRepository.save(createGenreDto);
-  }
-
-  findAll() {
-    return this.genreRepository.find();
-  }
-
-  findOne(id: number) {
-    return this.genreRepository.findOne({where:{id}});
-  }
-
-  async update(id: number, updateGenreDto: UpdateGenreDto) {
-    const genre= await this.genreRepository.findOne({where:{id}});
-
-    if(!genre){
-      throw new NotFoundException('존재하지 않는 장르입니다.')
-    }
-    //이름 중복이 있으면 안되니깐 확인해야하지 않을까?
-    if(updateGenreDto.name){
-      const dupicateGenre=await this.genreRepository.findOne({
-        where:{
-          name:updateGenreDto.name,
-          id:Not(id)
+    constructor(
+        //@InjectRepository(Genre) private readonly genreRepository:Repository<Genre>
+        private readonly prisma: PrismaService,
+    ) {}
+    async create(createGenreDto: CreateGenreDto) {
+        const genre = await this.prisma.genre.findUnique({
+            where: { name: createGenreDto.name },
+        });
+        if (genre) {
+            throw new NotFoundException('이미 존재하는 장르입니다.');
         }
-      });
-
-      if (dupicateGenre){
-        throw new ConflictException(`이미 존재하는 장르입니다. id는 ${dupicateGenre.id}`)
-      }
+        return this.prisma.genre.create({
+            data: createGenreDto,
+        });
     }
 
-    // console.log({...updateGenreDto})
-    await this.genreRepository.update({
-      id
-    },{
-      ...updateGenreDto
-    })
-    return await this.genreRepository.findOne({where:{id}})
-  }
+    findAll() {
+        return this.prisma.genre.findMany();
+    }
 
-  // 영화와 연결된 장르는 삭제하지 않도록 relation을 함께 조회해야한다!
-  async remove(id: number) {
-    const genre= await this.genreRepository.findOne({
-      where:{id},
-      relations:{movies:true}
-    })
-    if (!genre){
-      throw new NotFoundException('존재하지 않는 장르입니다.')
+    findOne(id: number) {
+        return this.prisma.genre.findUnique({ where: { id } });
     }
-    if(genre.movies.length>0){
-      throw new ConflictException(`영화에서 사용중인 장르는 삭제 할 수없습니다.
-        연결된 영화 ID:${genre.movies.map((movie)=>movie.id).join(',')}`)
+
+    async update(id: number, updateGenreDto: UpdateGenreDto) {
+        const genre = await this.prisma.genre.findUnique({ where: { id } });
+
+        if (!genre) {
+            throw new NotFoundException('존재하지 않는 장르입니다.');
+        }
+        //이름 중복이 있으면 안되니깐 확인해야하지 않을까?
+        if (updateGenreDto.name) {
+            const dupicateGenre = await this.prisma.genre.findUnique({
+                where: {
+                    name: updateGenreDto.name,
+                    NOT: { id },
+                    // id: Not(id),
+                },
+            });
+
+            if (dupicateGenre) {
+                throw new ConflictException(`이미 존재하는 장르입니다. id는 ${dupicateGenre.id}`);
+            }
+        }
+
+        // console.log({...updateGenreDto})
+        await this.prisma.genre.update({
+            where: { id },
+            data: updateGenreDto,
+        });
+        return await this.prisma.genre.findUnique({ where: { id } });
     }
-    
-    await this.genreRepository.delete(id);
-    return `${id}가 삭제되었습니다.`;
-  }
+
+    // 영화와 연결된 장르는 삭제하지 않도록 relation을 함께 조회해야한다!
+    async remove(id: number) {
+        const genre = await this.prisma.genre.findUnique({
+            where: { id },
+            include: {
+                movie_genres_genre: {
+                    select: { movieId: true },
+                },
+            },
+        });
+
+        if (genre && genre.movie_genres_genre.length > 0) {
+            throw new ConflictException(`영화에서 사용중인 장르는 삭제 할 수없습니다.
+        연결된 영화 ID:${genre.movie_genres_genre.map((movie) => movie.movieId).join(',')}`);
+        }
+
+        // if (!genre) {
+        //     throw new NotFoundException('존재하지 않는 장르입니다.');
+        // }
+        // if (genre.prisma.movie_genres_genre.length > 0) {
+        //     throw new ConflictException(`영화에서 사용중인 장르는 삭제 할 수없습니다.
+        // 연결된 영화 ID:${genre.movies.map((movie) => movie.id).join(',')}`);
+        // }
+
+        if (!genre) {
+            throw new NotFoundException('존재하지 않는 장르입니다.');
+        }
+
+        await this.prisma.genre.delete({ where: { id } });
+        return `${id}가 삭제되었습니다.`;
+    }
 }
